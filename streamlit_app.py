a1931bc51bd8407ff6181c92382dc2e23ce037e3
erzycerdon <127071126+erzycerdon@users.noreply.github.com>
2023-03-07 17:29:37 +0800

    Update streamlit_app.py
    removed indention

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,10 +58,10 @@ def get_fruit_load_list():
     
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    my_cnx.close()
-    streamlit.dataframe(my_data_rows)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_fruit_load_list()
+my_cnx.close()
+streamlit.dataframe(my_data_rows)
 #Allow the end user to add a fruit on the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
